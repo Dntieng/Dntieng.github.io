284562a56b29d8033df53cce31885141ce30e577
Dntieng <105242738+Dntieng@users.noreply.github.com>
2024-02-09 00:35:31 +0800

    Update RTO.py

--- RTO.py
+++ RTO.py
@@ -100,7 +100,3 @@ def save_to_github():
 
 with app.app_context():
     db.create_all()
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
